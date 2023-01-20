90f316cad74eec2adf954e51fa1ad999b1604238
Pradikshan <98381118+Pradikshan@users.noreply.github.com>
2023-01-20 22:43:05 +0530

    Add files via upload

--- Scripts/cart.js
+++ Scripts/cart.js
@@ -1,47 +1,28 @@
-var addToCartBtn = document.getElementById("addToCart");
-var removeFromCartBtn = document.getElementById("removeFromCart");
-var quantity = document.getElementById("quantity");
+var addToCartBtn = document.getElementsByClassName("addToCart");
+var removeFromCartBtn = document.getElementsByClassName("removeFromCart");
+var quantity = document.getElementsByClassName("quantity");
 
 // get the stored quantity from local storage
-if (localStorage.getItem("quantity")) {
-    quantity.innerHTML = "Quantity: " + localStorage.getItem("quantity");
+for(var i = 0; i < addToCartBtn.length; i++) {
+  if (localStorage.getItem("quantity" + i)) {
+      quantity[i].innerHTML = "Quantity: " + localStorage.getItem("quantity" + i);
+  }
+  addToCartBtn[i].addEventListener("click", decreaseQuantity);
+  removeFromCartBtn[i].addEventListener("click", increaseQuantity);
 }
 
-addToCartBtn.addEventListener("click", decreaseQuantity);
-removeFromCartBtn.addEventListener("click", increaseQuantity);
-
-function decreaseQuantity() {
-  var currentQuantity = parseInt(quantity.innerHTML.split(" ")[1]);
+function decreaseQuantity(e) {
+  var currentQuantity = parseInt(e.target.parentElement.previousElementSibling.innerHTML.split(" ")[1]);
     if (currentQuantity > 0) {
         currentQuantity--;
-        quantity.innerHTML = "Quantity: " + currentQuantity;
-        localStorage.setItem("quantity", currentQuantity);
-    }
-}
-var addToCartBtn = document.getElementById("addToCart");
-var removeFromCartBtn = document.getElementById("removeFromCart");
-var quantity = document.getElementById("quantity");
-
-// get the stored quantity from local storage
-if (localStorage.getItem("quantity")) {
-    quantity.innerHTML = "Quantity: " + localStorage.getItem("quantity");
-}
-
-addToCartBtn.addEventListener("click", decreaseQuantity);
-removeFromCartBtn.addEventListener("click", increaseQuantity);
-
-function decreaseQuantity() {
-  var currentQuantity = parseInt(quantity.innerHTML.split(" ")[1]);
-    if (currentQuantity > 0) {
-        currentQuantity--;
-        quantity.innerHTML = "Quantity: " + currentQuantity;
-        localStorage.setItem("quantity", currentQuantity);
+        e.target.parentElement.previousElementSibling.innerHTML = "Quantity: " + currentQuantity;
+        localStorage.setItem("quantity" + i, currentQuantity);
     }
 }
 
-function increaseQuantity() {
-  var currentQuantity = parseInt(quantity.innerHTML.split(" ")[1]);
+function increaseQuantity(e) {
+  var currentQuantity = parseInt(e.target.parentElement.previousElementSibling.innerHTML.split(" ")[1]);
     currentQuantity++;
-    quantity.innerHTML = "Quantity: " + currentQuantity;
-    localStorage.setItem("quantity", currentQuantity);
+    e.target.parentElement.previousElementSibling.innerHTML = "Quantity: " + currentQuantity;
+    localStorage.setItem("quantity" + i, currentQuantity);
 }
